6f348d07da36844f829aaae50a592175d7747d01
Aditya Jangir <adityajangir2299@gmail.com>
2023-05-18 09:55:56 +0530

    Add project files.

---
+++ ProcessManager/Helpers/ProcessHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace ProcessManager.Helpers;
+
+public static class ProcessHelper
+{
+    public static List<Process> FilterProcessByName(string processName)
+    {
+        var filteredProcess = new List<Process>();
+        var allProcesses = Process.GetProcesses();
+        foreach (var process in allProcesses)
+            if (process.ProcessName.StartsWith(processName, StringComparison.OrdinalIgnoreCase))
+                filteredProcess.Add(process);
+
+        return filteredProcess;
+    }
+}
---
+++ ProcessManager/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ProcessManager.Helpers;
+
+namespace ProcessManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Process> Processes { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            Processes = new ObservableCollection<Process>();
+            var currentProcess = Process.GetProcesses();
+            foreach (var process in currentProcess)
+                Processes.Add(process);
+            
+            ProcessTable.ItemsSource = Processes;
+        }
+
+
+
+        private void OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            Processes.Clear();
+            var filteredProcessName = NameFilter.Text;
+            var processes = ProcessHelper.FilterProcessByName(filteredProcessName);
+            foreach (var process in processes)
+                Processes.Add(process);
+            
+            ProcessTable.ItemsSource = Processes;
+        }
+
+        private void AddKillOrRestartProcessButton(object sender, SelectionChangedEventArgs e)
+        {
+            KillProcessButton.Visibility = Visibility.Visible;
+            RestartProcessButton.Visibility = Visibility.Visible;
+        }
+    }
+}
